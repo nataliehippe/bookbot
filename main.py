e90783e344f99bfbd9140522ec83d06d22eabdfc
Natalie_dev <natalie.alibegashvili@gmail.com>
2025-11-04 19:44:29 +0400

    get book name from the command

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from stats import get_num_words
-from stats import get_num_letters
-from stats import sort_characters
+import sys
+from stats import(
+    sort_characters,
+    get_num_letters,
+    get_num_words
+)
 
 def get_book_text(filePath):
     with open(filePath) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+
+    if len(sys.argv) < 2:
+        print("Error: expected stdout to contain Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     print(get_num_words(book_text, "me"))
 
     char_counts = get_num_letters(book_text)
